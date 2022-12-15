a12a1c2f17580a930a50b947a380135a0274c595
Dani Calvente <danicalvente@hotmail.com>
2022-12-15 13:00:25 +0100

    Regular Ball Super Ball

---
+++ 8-kyu/Regular Ball Super Ball.js
@@ -0,0 +1,25 @@
+/*
+Description:
+
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+class Ball {
+  constructor(ballType = "regular") {
+    this.ballType = ballType;
+  }
+}
+
+// A different aproach below.
+
+const Ball = function (ballType) {
+  this.ballType = ballType || "regular";
+};
