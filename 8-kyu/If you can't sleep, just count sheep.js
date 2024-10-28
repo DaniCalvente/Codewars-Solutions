93931054aacdf43a49248d2bf4f6f39f3c3da7ce
Dani Calvente <danicalvente@hotmail.com>
2024-10-28 13:16:55 +0100

    new kata

--- 8-kyu/If you can't sleep, just count sheep.js
+++ 8-kyu/If you can't sleep, just count sheep.js
@@ -6,3 +6,11 @@ If you can't sleep, just count sheeps!!
 Task:
 Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 */
+
+let countSheep = function (num) {
+  let listOfSheeps = "";
+  for (let i = 1; i <= num; i++) {
+    listOfSheeps += `${i} sheep...`;
+  }
+  return listOfSheeps;
+};
