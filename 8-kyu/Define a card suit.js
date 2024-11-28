14296c6c62dbe058e95188b0e68ec7e664cbcd92
Dani Calvente <danicalvente@hotmail.com>
2024-11-28 16:27:40 +0100

    New Kata

---
+++ 8-kyu/Define a card suit.js
@@ -0,0 +1,22 @@
+/*
+Description:
+
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+function defineSuit(card) {
+  return card[card.length - 1] == "♣"
+    ? "clubs"
+    : card[card.length - 1] == "♦"
+    ? "diamonds"
+    : card[card.length - 1] == "♥"
+    ? "hearts"
+    : "spades";
+}
